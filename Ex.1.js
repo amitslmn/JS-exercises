15fef64fc0fbc6a5b70ed82ebc818d8eb904b20d
amitslmn <amitslmn@gmail.com>
2021-08-29 18:58:39 +0300

    piece of code that computes the sum of digits of a 3-digit number

--- Ex.1.js
+++ Ex.1.js
@@ -1,1 +1,9 @@
-
+const num = 123
+let sum = 0
+function splitToDigit(n){
+    return (n + '').split('').map((i) => { return Number(i); })
+  }
+  var arr = splitToDigit(num) ;
+  sum = arr[0] +arr[1] +arr[2] ;
+  
+console.log(sum) 
